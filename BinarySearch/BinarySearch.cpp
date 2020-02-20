eae3159b0c8b4d7d4f66ac95eafc57b2f4f284fd
YJ Kim <39256000+kyjyeon@users.noreply.github.com>
2020-02-21 00:36:40 +0900

    BinarySearch 704

--- BinarySearch/BinarySearch.cpp
+++ BinarySearch/BinarySearch.cpp
@@ -1,1 +1,18 @@
-Solution
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int min = 0;
+        int max = nums.size() - 1;
+        while(min <= max){
+            int mid = min + (max - min)/2;
+            if(target == nums[mid])
+                return mid;
+            else if(target > nums[mid])
+                min = mid + 1;
+            else if(target < nums[mid])
+                max = mid - 1;
+        }
+        
+        return -1;
+    }
+};
