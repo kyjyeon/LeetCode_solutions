f03f65388edbb6eafdbd4666c1607a1de7b9e2e3
YJ Kim <39256000+kyjyeon@users.noreply.github.com>
2020-03-08 21:38:57 +0900

    DFS 100

---
+++ DFS/SameTree.cpp
@@ -0,0 +1,25 @@
+//Reference
+// Algorithm for the recursion:
+// 1)
+// If one of the node is NULL then return the equality result of p an q.
+// This boils down to if both are NULL then return true, 
+// but if one of them is NULL but not the other one then return false
+// 2)
+// At this point both root nodes represent valid pointers.
+// Return true if the root nodes have same value and 
+// the left tree of the roots are same (recursion)
+// and the right tree of the roots are same (recursion). 
+// Otherwise return false. 
+//
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p && !q)    return true;
+        if(!p || !q)    return false;
+        return p->val==q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+
+
