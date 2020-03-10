1d246a834c30c2f0f7cd704674f1ac01c6eeadb1
kyjyeon <kyjyeon93@gmail.com>
2020-03-11 01:32:36 +0900

    DFS but used BFS

---
+++ DFS/CourseSchedule.cpp
@@ -0,0 +1,39 @@
+//Reference 
+//Queue usuage can be very helpful for recursive
+//Idea of starting from backwards is importing (Starting from the last course that we have to take)
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<set<int>> g(numCourses);    
+        vector<int> in(numCourses);
+
+        for (auto x : prerequisites) {
+            g[x[0]].insert(x[1]);
+            in[x[1]]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (in[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        int vis = 0;
+
+        while (!q.empty()) {
+            int x = q.front();
+            q.pop();
+            vis++;
+            for (int i : g[x]) {
+                in[i]--;
+                if (in[i] == 0) {
+                    q.push(i);
+                }
+            }
+        }
+
+        return (vis == numCourses);
+
+    }
+};
